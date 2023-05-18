9256aefcd92207fb1280f9586c0800712363f7fb
Aghatareis <ma0929959@gmail.com>
2023-05-18 17:05:41 -0300

    Initial commit

---
+++ app/src/main/java/com/example/art/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.art
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.art.ui.theme.ARTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ARTTheme {
+                    CardNumberOne()
+                }
+            }
+        }
+    }
+
+@Preview
+@Composable
+fun CardNumberOne(){
+    Surface (
+        modifier = Modifier
+            .fillMaxSize(),
+        color = Color.White
+            ){
+        Column (
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(45.dp)
+                )
+        {
+            NumberImage(
+                painter = painterResource(id = R.drawable.sakuna)
+            )
+
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier
+                    .padding(65.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Nome Do Autor"
+                )
+
+                Text(
+                    text = "Desrição"
+                )
+            }
+        }
+        Row (
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .padding(50.dp)
+        )
+        {
+            Button(
+                onClick = { /*TODO*/ })
+            {
+                Text(text = "Anterior")
+            }
+
+            Button(
+                onClick = { /*TODO*/ })
+            {
+                Text(text = "Avançar")
+            }
+        }
+    }
+}
+
+@Composable
+fun NumberImage(painter: Painter){
+    Image(
+        painter = painter ,
+        contentDescription = null,
+        modifier = Modifier
+            .padding(top = 25.dp)
+            .fillMaxWidth()
+
+
+    )
+}
